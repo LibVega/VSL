c8dc60fc31e69a0a27f1eb5c62be01fb6ba013fc
Sean Moss <moss.seank@gmail.com>
2020-12-20 14:56:04 -0500

    Basic ANTLR grammars and generation scripts

---
+++ generated/README.md
@@ -0,0 +1,7 @@
+## Generated
+
+This is the output directory for the generated ANTLR Lexer/Parser for Polaris.
+
+All files here are deterministically generated, and are therefore ignored by Git.
+
+Java is required to generate these files, and the generation scripts can be found in ../grammar.
---
+++ grammar/generate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+# This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+# the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+#
+
+# This is the syntax parser/lexer generator script for Unix.
+
+java -jar ../tools/antlr-4.9-complete.jar \
+    -no-listener                          \
+    -visitor                              \
+    -o ../generated                       \
+    -package grammar                      \
+    -Xexact-output-dir                    \
+    -Dlanguage=Cpp                        \
+    PLSLLexer.g4
+
+java -jar ../tools/antlr-4.9-complete.jar \
+    -no-listener                          \
+    -visitor                              \
+    -o ../generated                       \
+    -package grammar                      \
+    -Xexact-output-dir                    \
+    -Dlanguage=Cpp                        \
+    PLSL.g4
