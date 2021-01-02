f08c9db9a1e7fbe2ca7a0b68d4ec927f8d1182af
Sean Moss <moss.seank@gmail.com>
2021-01-02 15:24:21 -0500

    Update copyright year

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 @rem
-@rem Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+@rem Microsoft Public License (Ms-PL) - Copyright (c) 2020-2021 Sean Moss
 @rem This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
 @rem the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
 @rem
